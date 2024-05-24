21546ebbeed0dd950de1cbfca6fc0d43d7729c13
AlexAttomo <104031928+Alex-MM23@users.noreply.github.com>
2024-05-24 12:59:10 +0200

    Texto dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -27,4 +27,29 @@ export class DashboardComponent implements OnInit {
     this._productService.addProduct(product)
   }
 
+  showContent(index: number) {
+    // Primero ocultamos todos los textos desplegables
+    const allTextElements = document.querySelectorAll('.desplegable');
+    allTextElements.forEach(element => {
+      element.classList.remove('mostrar');
+    });
+
+    // Quitamos la clase 'activo' de todos los botones
+    const allButtons = document.querySelectorAll('.botones h4');
+    allButtons.forEach(button => {
+      button.classList.remove('activo');
+    });
+
+    // Mostramos el texto correspondiente y añadimos la clase 'activo' al botón
+    const selectedTextElement = document.getElementById(`txts_${index}`);
+    const selectedButton = document.getElementById(`mostrar_${index}`);
+    
+    if (selectedTextElement) {
+      selectedTextElement.classList.add('mostrar');
+    }
+    if (selectedButton) {
+      selectedButton.classList.add('activo');
+    }
+  }
+
 }
--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-tienda',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TiendaComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this._productService.getProducts().subscribe(data => {
+      this.products = data;
+    })
+  }
+
+  addToCart(product: Product) {
+
+    this._productService.addProduct(product)
   }
 
 }
